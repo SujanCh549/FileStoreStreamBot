9992a07f1effe002695d86a3970d50c481227547
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2023-09-27 14:55:58 +0545

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -13,7 +13,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"<b>ꜰɪʟᴇꜱ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 30 ᴍɪɴᴜᴛᴇꜱ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ. ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴀɴᴅ ꜱᴀᴠᴇ ᴛʜᴇᴍ.</b>\n\n<b>❣️ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ❣️:- @FilmyHub_Adda</b>",
+            f"ꜰɪʟᴇꜱ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 30 ᴍɪɴᴜᴛᴇꜱ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ. ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴀɴᴅ ꜱᴀᴠᴇ ᴛʜᴇᴍ.\n\n❣️ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ❣️:- @FilmyHub_Adda",
             disable_web_page_preview=True,
             quote=True
         )
