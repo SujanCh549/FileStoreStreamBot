a67bdd63243b3eb16dfe4de078212c272f03cc32
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-01-13 21:04:09 +0545

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,5 +1,3 @@
-# (c) @PredatorHackerzZ
-
 import asyncio
 from typing import (
     Union
@@ -32,7 +30,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩](https://t.me/Sujan_Ch).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Owner](https://t.me/Sujan_Ch).",
                 disable_web_page_preview=True
             )
             return 400
@@ -61,7 +59,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩](https://t.me/Sujan_Ch).",
+            text="Something went Wrong. Contact my [Owner](https://t.me/Sujan_Ch).",
             disable_web_page_preview=True
         )
         return 200
