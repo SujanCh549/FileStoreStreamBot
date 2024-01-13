c880d376ced4c275093ecdacbc244c4c4de95115
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-01-13 23:28:43 +0545

    Add files via upload

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -1,3 +1,5 @@
+# (c) @LazyDeveloperr
+
 import asyncio
 import requests
 import string
@@ -25,8 +27,8 @@ def get_short(url):
         return rjson["shortenedUrl"]
     else:
         return url
-
-    
+        
+        
 async def forward_to_channel(bot: Client, message: Message, editable: Message):
     try:
         __SENT = await message.forward(Config.DB_CHANNEL)
@@ -61,17 +63,17 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             text=message_ids_str,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
+                InlineKeyboardButton("❌Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
         short_link = get_short(share_link)
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: <code>{short_link}</code> \n\n"
             f"Just Click the link to get your files!",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("♻️Original Link", url=share_link),
-                  InlineKeyboardButton("🔗Short Link", url=short_link)]]
+                [[InlineKeyboardButton("🔗️Original Link", url=share_link),
+                  InlineKeyboardButton("💲Short Link", url=short_link)]]
             ),
             disable_web_page_preview=True
         )
@@ -79,12 +81,13 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♻️Original Link", url=short_link),
-                                                InlineKeyboardButton("🔗Short Link", url=share_link)]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💲Short Link", url=short_link),
+                                                InlineKeyboardButton("🔗Original Link", url=share_link)]])
         )
-
-        if (Config.LAZY_MODE == True):
-
+        # ✧ Bina soche smjhe code edit mt krna wrna error dhundne mei umrr beet jaayega.
+        # ✧ source code upgraded by The sir LazyDeveloper 
+        # ✧ Don't remove credit ✧ @LazyDeveloper ✧
+        if(Config.LAZY_MODE == True):
             thumbs= message.video.thumbs[0]
             file_id= thumbs.file_id
             lazy_channel = int(Config.LAZY_CHANNEL)
@@ -135,9 +138,9 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 k = await message.reply_text(text=cptz)
                 await asyncio.sleep(30)
                 await k.delete()
-                
+        
     except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
+        await editable.edit(f"ꜱᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...\n\n**Error:** `{err}`")
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
@@ -149,7 +152,6 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             )
         )
 
-
 async def save_media_in_channel(bot: Client, editable: Message, message: Message):
     try:
         forwarded_msg = await message.forward(Config.DB_CHANNEL)
@@ -157,20 +159,22 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(file_er_id)}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
         short_link = get_short(share_link)
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
             f"Here is the Permanent Link of your file: <code>{short_link}</code> \n\n"
             "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
-               [[InlineKeyboardButton("♻️Original Link", url=share_link),
-                  InlineKeyboardButton("🔗Short Link", url=short_link)]]
+               [[InlineKeyboardButton("🔗️Original Link", url=share_link),
+                  InlineKeyboardButton("💲Short Link", url=short_link)]]
             ),
             disable_web_page_preview=True
         )
-
-        if (Config.LAZY_MODE == True):
+        # ✧ Bina soche smjhe code edit mt krna wrna error dhundne mei umrr beet jaayega.
+        # ✧ source code upgraded by The sir LazyDeveloper 
+        # ✧ Don't remove credit ✧ @LazyDeveloper ✧
+        if(Config.LAZY_MODE == True):
             thumbs= message.video.thumbs[0]
             file_id= thumbs.file_id
             lazy_channel = int(Config.LAZY_CHANNEL)
@@ -221,7 +225,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
                 k = await message.reply_text(text=cptz)
                 await asyncio.sleep(40)
                 await k.delete()
-                
+
     except FloodWait as sl:
         if sl.value > 45:
             print(f"Sleep of {sl.value}s caused by FloodWait ...")
@@ -239,7 +243,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             )
         await save_media_in_channel(bot, editable, message)
     except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
+        await editable.edit(f"ꜱᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...\n\n**Error:** `{err}`")
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
             text="#ERROR_TRACEBACK:\n"
@@ -252,4 +256,3 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
                 ]
             )
         )
-        
