4bb23e3a2e9cac2c601ac875dca9e567cfa9f478
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-01-13 21:14:41 +0545

    Update __init__.py

--- lazybot/__init__.py
+++ lazybot/__init__.py
@@ -1,11 +1,3 @@
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-    # Born to make history @LazyDeveloper !
-
-    # Thank you LazyDeveloper for helping us in this Journey
-    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-
-    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
 import logging
 import logging.config
 logging.config.fileConfig('logging.conf')
