555c18af5a3ff541eaab190e6c50675de484aae7
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-01-13 21:52:28 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -540,21 +540,4 @@ async def button(bot: Client, cmd: CallbackQuery):
         await cmd.answer()
     except QueryIdInvalid: pass
 
-    async def main_function():
-    if ON_HEROKU:
-        asyncio.create_task(ping_server())
-
-    me = await Bot.get_me()
-    Bot.username = '@' + me.username
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
-    await web.TCPSite(app, bind_address, PORT).start()
-    await idle()
-
-if __name__ == '__main__':
-    try:
-        loop.run_until_complete(main_function())
-    except KeyboardInterrupt:
-        logging.info('Service Stopped')
-        
+    Bot.run()
