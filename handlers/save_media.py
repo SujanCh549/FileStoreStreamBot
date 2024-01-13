c7f20675117885a14e8359e3d09c3626bf932dc2
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-01-13 20:58:06 +0545

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -84,6 +84,59 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♻️Original Link", url=short_link),
                                                 InlineKeyboardButton("🔗Short Link", url=share_link)]])
         )
+
+    if(Config.LAZY_MODE == True):
+            thumbs= message.video.thumbs[0]
+            file_id= thumbs.file_id
+            lazy_channel = int(Config.LAZY_CHANNEL)
+            location=await bot.download_media(file_id)
+            lazypost_channel_username = (Config.LP_CHANNEL_USRNM)
+            lazypost_ch_admin_usrnm = (Config.LPCH_ADMIN_USRMN)
+            main_channel_username = (Config.LP_BTN_MAIN_CH_USRNM)
+            main_btn_link = f"https://telegram.me/{main_channel_username}"
+            file_name = message.caption
+            caption_z = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_za = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zab = f"{file_name}\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zabi = f"{file_name}\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazy_dev = f"+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazypost_custom_template = f"{(Config.LP_CUSTOM_TEMPLATE)}\n\n{lazy_dev} ♥️"
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("⎝⎝✧ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ✧⎠⎠", url=share_link)],
+                 [InlineKeyboardButton("⚡️✧ ɢᴇᴛ ʙᴀᴛᴄʜ ꜰɪʟᴇꜱ ✧⚡️", url=share_link)],
+                ]
+            )
+            main_btn=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("⎝⎝✧ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ✧⎠⎠", url=share_link)],
+                 [InlineKeyboardButton("⚡️✧ ɢᴇᴛ ʙᴀᴛᴄʜ ꜰɪʟᴇꜱ ✧⚡️", url=share_link)],
+                 [InlineKeyboardButton("•⊹٭𝚓𝚘𝚒𝚗 𝚖𝚊𝚒𝚗 𝚌𝚑𝚊𝚗𝚗𝚎𝚕٭⊹•", url=main_btn_link)]
+                ]
+            )
+            # ✧ Here is the condition for sending POST in movie channel
+            if(Config.LP_CUSTOM_TEMPLATE):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=reply_markup)
+            elif(Config.LP_CUSTOM_TEMPLATE and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=reply_markup)
+            elif(Config.LP_CHANNEL_USRNM and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=reply_markup)
+            elif(Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=main_btn)
+            elif(Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=reply_markup)
+            else:
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zabi,reply_markup=reply_markup)
+                # ✧ Please don't add unnescesary things here >[LazyDeveloper]
+                cptz = f"🥷\n\nღ♡ **𝘗𝘖𝘚𝘛 𝘜𝘗𝘓𝘖𝘈𝘋𝘌𝘋 𝘖𝘕 𝘊𝘏𝘈𝘕𝘕𝘌𝘓 𝘚𝘜𝘊𝘊𝘌𝘚𝘚𝘍𝘜𝘓𝘓𝘠**✅\n\n**• NOTE: **ᴘʟᴇᴀꜱᴇ ᴏɴʟʏ ᴄʟɪᴄᴋ ->** ɢᴇᴛ ʙᴀᴛᴄʜ & ᴘᴏꜱᴛ **<- ᴏɴ ᴀɴʏ ᴏɴᴇ ᴏꜰ ᴛʜᴇ ᴀʙᴏᴠᴇ ᴍᴇꜱꜱᴀɢᴇꜱ\n\nN͢O͢ o͢f͢ c͢l͢i͢c͢k͢ = n͢o͢ o͢f͢ p͢o͢s͢t͢s͢\n\n• ʙᴇ ᴄᴀʀᴇꜰᴜʟʟ - ʏᴏᴜ ᴀʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ ɪɴ ʟᴀᴢʏ_ᴍᴏᴅᴇ\n• @ʟᴀᴢʏᴅᴠᴇʟᴏᴘᴇʀ"
+                k = await message.reply_text(text=cptz)
+                await asyncio.sleep(30)
+                await k.delete()
+                
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
         await bot.send_message(
@@ -117,6 +170,59 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             ),
             disable_web_page_preview=True
         )
+
+if(Config.LAZY_MODE == True):
+            thumbs= message.video.thumbs[0]
+            file_id= thumbs.file_id
+            lazy_channel = int(Config.LAZY_CHANNEL)
+            location=await bot.download_media(file_id)
+            lazypost_channel_username = (Config.LP_CHANNEL_USRNM)
+            lazypost_ch_admin_usrnm = (Config.LPCH_ADMIN_USRMN)
+            main_channel_username = (Config.LP_BTN_MAIN_CH_USRNM)
+            main_btn_link = f"https://telegram.me/{main_channel_username}"
+            file_name = message.caption
+            caption_z = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_za = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zab = f"{file_name}\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zabi = f"{file_name}\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazy_dev = f"+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazypost_custom_template = f"{(Config.LP_CUSTOM_TEMPLATE)}\n\n{lazy_dev} ♥️"
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("⎝⎝✧ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ✧⎠⎠", url=share_link)],
+                 [InlineKeyboardButton("ミ★ GΞΓ FILΞ ★彡", url=share_link)],
+                ]
+            )
+            main_btn=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("⎝⎝✧ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ✧⎠⎠", url=share_link)],
+                 [InlineKeyboardButton("ミ★ GΞΓ FILΞ ★彡", url=share_link)],
+                 [InlineKeyboardButton("•⊹٭𝚓𝚘𝚒𝚗 𝚖𝚊𝚒𝚗 𝚌𝚑𝚊𝚗𝚗𝚎𝚕٭⊹•", url=main_btn_link)]
+                ]
+            )
+            # ✧ Here is the condition for sending POST in movie channel ✧ LazyDeveloper ✧
+            if(Config.LP_CUSTOM_TEMPLATE):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=reply_markup)
+            elif(Config.LP_CUSTOM_TEMPLATE and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=reply_markup)
+            elif(Config.LP_CHANNEL_USRNM and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=reply_markup)
+            elif(Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=main_btn)
+            elif(Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=reply_markup)
+            else:
+                # ✧ Please don't add unnescesary things here >[LazyDeveloper]
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zabi,reply_markup=reply_markup)
+                cptz = f"🥷\n\nღ♡ **𝘗𝘖𝘚𝘛 𝘜𝘗𝘓𝘖𝘈𝘋𝘌𝘋 𝘖𝘕 𝘊𝘏𝘈𝘕𝘕𝘌𝘓 𝘚𝘜𝘊𝘊𝘌𝘚𝘚𝘍𝘜𝘓𝘓𝘠**✅\n\n• ʙᴇ ᴄᴀʀᴇꜰᴜʟʟ - ʏᴏᴜ ᴀʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ ɪɴ ʟᴀᴢʏ_ᴍᴏᴅᴇ\n• @ʟᴀᴢʏᴅᴠᴇʟᴏᴘᴇʀ"
+                k = await message.reply_text(text=cptz)
+                await asyncio.sleep(40)
+                await k.delete()
+                
     except FloodWait as sl:
         if sl.value > 45:
             print(f"Sleep of {sl.value}s caused by FloodWait ...")
